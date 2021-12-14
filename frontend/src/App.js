1eeafea3600b5b6710da4c59f7589e3e9229b852
sashanksilwal <sashanksilwal@hotmail.com>
2021-12-14 09:39:39 -0500

    debugging the error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,8 +10,8 @@ import UserProfile from "./containers/userProfile/userProfile";
 import Header from "./components/header/header";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-import CircularProgress from "@mui/material/CircularProgress";
-import Box from "@mui/material/Box";
+// import CircularProgress from "@mui/material/CircularProgress";
+// import Box from "@mui/material/Box";
 import Post from "./containers/Post/post";
 import Home from "./containers/homePage/homePage";
 
@@ -22,21 +22,18 @@ const FirebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDING_ID,
   appId: process.env.REACT_APP_APP_ID,
-  // apiKey: "AIzaSyAmNfmot7JAWfRj8fETJrWGS2UnL0f-E3U",
-  // authDomain: "final-project-ac590.firebaseapp.com",
-  // projectId: "final-project-ac590",
-  // storageBucket: "final-project-ac590.appspot.com",
-  // messagingSenderId: "997232705866",
-  // appId: "1:997232705866:web:37e6d2f362e241f20141b3",
 };
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [loading, setLodaing] = useState(false);
+  const [loggedIn, setLoggedIn] = useState();
+  const [loading, setLoading] = useState(false);
   const [userInformation, setUserInformation] = useState({});
   const [appInitialized, setAppInitialized] = useState(false);
   const [errors, setErrors] = useState();
 
+  console.log(loading, "Loading");
+  console.log(loggedIn, "Loggedin");
+  console.log(appInitialized, "appInitialized");
   useEffect(() => {
     initializeApp(FirebaseConfig);
     setAppInitialized(true);
@@ -53,7 +50,7 @@ function App() {
           setUserInformation({});
           setLoggedIn(false);
         }
-        setLodaing(false);
+        setLoading(false);
       });
     }
   }, [appInitialized]);
@@ -71,124 +68,120 @@ function App() {
       });
   }
 
-  if (appInitialized & !loading) {
-    return (
-      <div>
-        <Header logout={logout} loggedIn={loggedIn} />
-        {errors && <p className="Error">{errors}</p>}
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path="/login"
-              element={
-                !loggedIn ? (
-                  <Login
-                    setLoggedIn={setLoggedIn}
-                    setUserInformation={setUserInformation}
-                    setError={setErrors}
-                  />
-                ) : (
-                  <Navigate to="/home" />
-                )
-              }
-            />
-            <Route
-              path="/createuser"
-              element={
-                !loggedIn ? (
-                  <CreateUser
-                    setLoggedIn={setLoggedIn}
-                    setUserInformation={setUserInformation}
-                    setError={setErrors}
-                  />
-                ) : (
-                  <Navigate to="/home" />
-                )
-              }
-            />
-            <Route
-              path="/user/:id"
-              element={
-                loggedIn ? (
-                  <UserProfile userInformation={userInformation} />
-                ) : (
-                  <Login
-                    setLoggedIn={setLoggedIn}
-                    setUserInformation={setUserInformation}
-                    setError={setErrors}
-                  />
-                )
-              }
-            />
-
-            <Route
-              path="/addpost"
-              element={
-                loggedIn ? (
-                  <AddPost userInformation={userInformation} />
-                ) : (
-                  <Login
-                    setLoggedIn={setLoggedIn}
-                    setUserInformation={setUserInformation}
-                    setError={setErrors}
-                  />
-                )
-              }
-            />
-            <Route
-              path="/post/:slug"
-              element={
-                loggedIn ? (
-                  <Post />
-                ) : (
-                  <Login
-                    setLoggedIn={setLoggedIn}
-                    setUserInformation={setUserInformation}
-                    setError={setErrors}
-                  />
-                )
-              }
-            />
-            <Route path="/" element={<Home />} />
-            <Route
-              path="/home"
-              element={
-                loggedIn ? (
-                  <Dashboard />
-                ) : (
-                  <Login
-                    setLoggedIn={setLoggedIn}
-                    setUserInformation={setUserInformation}
-                    setError={setErrors}
-                  />
-                )
-              }
-            />
-            <Route
-              path="/:anything"
-              element={
-                loggedIn ? (
-                  <Dashboard />
-                ) : (
-                  <Login
-                    setLoggedIn={setLoggedIn}
-                    setUserInformation={setUserInformation}
-                    setError={setErrors}
-                  />
-                )
-              }
-            />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    );
-  } else {
-    return (
-      <Box sx={{ display: "flex" }}>
-        <CircularProgress />
-      </Box>
-    );
+  if (loading || !appInitialized || loggedIn == null) {
+    return null;
   }
+
+  return (
+    <div>
+      <Header logout={logout} loggedIn={loggedIn} />
+      {errors && <p className="Error">{errors}</p>}
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              !loggedIn ? (
+                <Login
+                  setLoggedIn={setLoggedIn}
+                  setUserInformation={setUserInformation}
+                  setError={setErrors}
+                />
+              ) : (
+                <Navigate to="/home" />
+              )
+            }
+          />
+          <Route
+            path="/createuser"
+            element={
+              !loggedIn ? (
+                <CreateUser
+                  setLoggedIn={setLoggedIn}
+                  setUserInformation={setUserInformation}
+                  setError={setErrors}
+                />
+              ) : (
+                <Navigate to="/home" />
+              )
+            }
+          />
+          <Route
+            path="/user/:id"
+            element={
+              loggedIn ? (
+                <UserProfile userInformation={userInformation} />
+              ) : (
+                <Navigate to="/login" />
+                // <Login
+                //   setLoggedIn={setLoggedIn}
+                //   setUserInformation={setUserInformation}
+                //   setError={setErrors}
+                // />
+              )
+            }
+          />
+
+          <Route
+            path="/addpost"
+            element={
+              loggedIn ? (
+                <AddPost userInformation={userInformation} />
+              ) : (
+                <Navigate to="/login" />
+                // <Login
+                //   setLoggedIn={setLoggedIn}
+                //   setUserInformation={setUserInformation}
+                //   setError={setErrors}
+                // />
+              )
+            }
+          />
+          <Route
+            path="/post/:slug"
+            element={
+              loggedIn ? (
+                <Post />
+              ) : (
+                <Navigate to="/login" />
+                // <Login
+                //   setLoggedIn={setLoggedIn}
+                //   setUserInformation={setUserInformation}
+                //   setError={setErrors}
+                // />
+              )
+            }
+          />
+
+          <Route
+            path="/home"
+            element={
+              loggedIn ? (
+                <Dashboard />
+              ) : (
+                <Navigate to="/login" />
+                // <Login
+                //   setLoggedIn={setLoggedIn}
+                //   setUserInformation={setUserInformation}
+                //   setError={setErrors}
+                // />
+              )
+            }
+          />
+
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
+//   else {
+//     return (
+//       <Box sx={{ display: "flex" }}>
+//         <CircularProgress />
+//       </Box>
+//     );
+//   }
+// }
 
 export default App;
